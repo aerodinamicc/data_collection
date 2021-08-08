043dad986a80fa12d2bc85f77c1d3876a42b795a
z <aerodinamicbg@gmail.com>
2021-08-08 22:14:39 +0300

    sectors

--- stocks/stocks.sql
+++ stocks/stocks.sql
@@ -34,25 +34,53 @@ $$ language SQL IMMUTABLE;
 -- Price below historic prices averaged across sectors
 ------
 
-select sector, avg(percent_lower) as avg_percent_lower, count(*) as count_
-from (
-	select 
-		*,
-		ROUND(100 - ((high/max_ever) * 100.), 2) as percent_lower
-	from (
-		select 
-			sc.name, 
-			sc.sector,
-			pa.symbol, 
-			pa.at_date,
-			pa.volume, 
-			pa.high, 
-			max(high) over (partition by symbol) as max_ever
-		from price_actions pa
-		left join (select *, details->>'sector' as sector from sp500_companies) sc using(symbol)
-	) v
-	where at_date = '2021-08-06'::date
-) d
+with src as (
+select 
+	sc.name, 
+	sc.sector,
+	pa.symbol, 
+	pa.at_date,
+	pa.volume, 
+	pa.high, 
+	max(high) over (partition by symbol) as max_ever
+from price_actions pa
+left join (select *, details->>'sector' as sector from sp500_companies) sc using(symbol)
+), 
+perc as (
+select 
+	*,
+	ROUND(100 - ((high/max_ever) * 100.), 2) as percent_lower
+from src 
+where at_date = '2021-08-06'::date 
+	OR high = max_ever
+),
+mid as (
+select 
+ p1.*,
+ date_part('day', '2021-08-06'::timestamp - coalesce(p2.at_date, p1.at_date)::timestamp) as days_since_max_date,
+ coalesce(p2.at_date, p1.at_date) as max_date
+from perc p1
+left join perc p2 using(symbol)
+where p1.at_date = '2021-08-06'::date
+and p2.at_date <> '2021-08-06'::date
+),
+fin as (
+select 
+	sector, 
+	count(*) as count_,
+	round(avg(percent_lower)) as avg_percent_lower, 
+	round(stddev(percent_lower), 2) as std_percent_lower,
+	round(avg(days_since_max_date)) as avg_days_since_max_date,
+	round(stddev(days_since_max_date), 2) as std_days_since_max_date
+from mid
+where sector is not null
 group by 1
-order by 2 desc
-;
+order by 3 desc
+)
+select * 
+--into ticker_stats from mid
+from fin;
+
+select * 
+from ticker_stats 
+where sector = 'Consumer Cyclical'
